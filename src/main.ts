c5355cd06134502a9e6ffc0436f157404930d437
felipejhordan <felipejordan.alves@gmail.com>
2022-02-14 22:49:56 -0300

    feat: configure fastify in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { FastifyAdapter,
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+    cors: true
+  });
+
   await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
